8cd4b7fb1d1bb194e703f9d67729636aa16bc1ea
Matthew Davis <github@mdavis.xyz>
2019-12-15 17:08:16 +1100

    fix license issue

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
   download_url = 'https://github.com/mlda065/bbp/archive/v_01.tar.gz',
   keywords = ['boto3', 'boto', 'AWS', 'Amazon', 'paginator', 'pagination', 'page', 'api', 'cloud'],
   install_requires=['boto3'],
-  classifiers=[
+  classifiers=[ # https://pypi.org/classifiers/
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPLv3 License',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
