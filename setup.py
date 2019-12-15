f168a255d51f823678c72d493cdba4dd4c42c1d0
Matthew Davis <github@mdavis.xyz>
2019-12-15 17:11:14 +1100

    delete troublesome tag

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
